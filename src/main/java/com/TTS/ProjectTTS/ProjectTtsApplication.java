066afdea7b60eddf6eedb98eef79fffee8882916
MatthewADIAN <672018055@student.uksw.edu>
2020-11-07 16:52:06 +0700

    My first commit

---
+++ src/main/java/com/TTS/ProjectTTS/ProjectTtsApplication.java
@@ -0,0 +1,13 @@
+package com.TTS.ProjectTTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TTS/ProjectTTS/ProjectTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTS.ProjectTTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
